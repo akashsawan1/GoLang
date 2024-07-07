6c2c07bc7fbbc00f23aa2abda03323daed3149ee
Akash Sawan <akash27274@gmail.com>
2024-07-07 16:54:54 +0530

    progress 1

--- UserInput/main.go
+++ UserInput/main.go
@@ -12,26 +12,39 @@ func main() {
 	fmt.Println("We have total of" , conferenceTickets , "tickets and " , remainingTickets, " are remaining")
 	fmt.Println("Get your tickets here to attend")
 
+	for {
+		var userName string
+		var lastName string
+		var email string
+		var userTicket uint
+		bookings := []string{}
+		
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&userName)
 
-	var userName string
-	var lastName string
-	var email string
-	var userTicket uint
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+
+		fmt.Println("How many Tickets you want to book: ")
+		fmt.Scan(&userTicket)
+
+		remainingTickets = remainingTickets - userTicket
+
+		bookings = append(bookings, userName + " " + lastName)
+		
+		fmt.Printf("The whole array: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("Array type : %T\n", bookings)
+		fmt.Printf("array length : %v\n", len(bookings))
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", userName, lastName, userTicket, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
+
 	
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&userName)
-
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
-
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
-
-	fmt.Println("How many Tickets you want to book: ")
-	fmt.Scan(&userTicket)
-
-	remainingTickets = remainingTickets - userTicket
-
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", userName, lastName, userTicket, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
